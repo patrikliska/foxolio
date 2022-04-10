c03754166a0b7697e5bd206514be7b133517602f
Patrik Liška <patrik.liska@chyron.com>
2022-04-10 21:05:37 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import { auth, provider } from './utils/firebase';
+import { signInWithPopup } from 'firebase/auth';
 
-function App() {
+import Portfolio from './pages/Portfolio';
+import Home from './pages/Home';
+
+const App = () => {
+  const [isAuth, setIsAuth] = useState(
+    JSON.parse(localStorage.getItem('isAuth') ?? 'false')
+  );
+  const [avatar, setAvatar] = useState(
+    localStorage.getItem('userAvatar') ?? ''
+  );
+
+  const googleSignIn = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem('isAuth', 'true');
+      localStorage.setItem(
+        'userAvatar',
+        result.user.providerData[0].photoURL ?? ''
+      );
+      setAvatar(result.user.providerData[0].photoURL ?? '');
+      setIsAuth(true);
+    });
+  };
+
+  const signOut = () => {
+    localStorage.setItem('isAuth', 'false');
+    localStorage.setItem('userAvatar', '');
+  };
+
+  console.log('avatar', avatar);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App' style={{ textAlign: 'center' }}>
+      <nav
+        style={{
+          width: '100%',
+          backgroundColor: '#282c34',
+          padding: '20px',
+          textAlign: 'left',
+        }}
+      >
+        <Link
+          to='/'
+          style={{ color: 'white', textDecoration: 'none', margin: '0 8px' }}
         >
-          Learn React
-        </a>
-      </header>
+          Home
+        </Link>
+        <Link
+          to='/portfolio'
+          style={{ color: 'white', textDecoration: 'none', margin: '0 8px' }}
+        >
+          Portfolio
+        </Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='portfolio' element={<Portfolio />} />
+      </Routes>
+      <button disabled={isAuth} onClick={googleSignIn}>
+        Sign in
+      </button>
+      <img src={avatar} alt='Avatar' />
+      <button disabled={!isAuth} onClick={signOut}>
+        Sign out
+      </button>
     </div>
   );
-}
+};
 
 export default App;
